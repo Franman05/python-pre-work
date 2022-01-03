bf72172d5855f248c63bc00292a709c0cfa5077a
Francis Kentris <franciskentris@yahoo.com>
2022-01-02 18:43:41 -0700

    first commit

---
+++ pre-work questions.py
@@ -0,0 +1,43 @@
+# Question 1:
+# Write a function to print "hello_USERNAME!" USERNAME is the input of the function. The first line of the code has been defined below.
+
+def hello_name(user_name):
+   print(user_name + "hello_USERNAME!")
+   hello_name()
+
+
+# Quetion 2
+# Write a python function, first_odds that prints the odd numbers from 0-100 and returns nothing.
+
+def first_odds():
+    () = 1
+    while () > 100:
+        print()
+
+
+# Question 3
+# Please write a python function, max_num_in_list to return the max number of a given list. The first line of the code has been defined below.
+
+def max_num_in_list(a_list):
+     a_list = ["20, 30, 40, 50"]
+print(max_num_in_list[3])
+
+# Question 4
+# Write a function to return if the given year is a leap year. A leap is divisible by by 4, but not divisible by 100, unless it is also divisible by 400. the return should be boolean type (true\false).
+
+def is_leap_year(a_year):
+    print(bool(400))
+    print(bool(100))
+    print(bool(4))
+
+# Question 5
+# Write a function to check to see if all numbers in list are consecutive numbers. For example, [2,3,4,5,6,7] are consecutive numbers, but [1,2,4,5] are not consecutive numbers. The returns should be boolean type.
+
+def is_consecutive(a_list):
+    my_string=" "
+    for element in a_list:
+        my_string += element[0]
+    return my_string
+list_num = ["10","15","20", "25","30"]
+print(is_consecutive(list_num))
+
